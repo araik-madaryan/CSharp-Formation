10c251bbec3df0ef5e077d9ea9779d364159cce4
unknown <contact@araikmadaryan.dev>
2020-08-17 00:36:17 +0200

    Ajout du dossier : Dictionnaires

---
+++ Dictionnaires/Dictionnaires/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionnaires
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Déclaration d'un dictionnaire avec des nombres.
+            Dictionary<int, string> nombres = new Dictionary<int, string>()
+            {
+                { 1, "un" },
+                { 2, "deux" },
+                { 3, "trois" }
+            };
+
+            nombres.Add(4, "quatre");
+
+            foreach(KeyValuePair<int, string> element in nombres)
+            {
+                Console.WriteLine($"{element.Key} - {element.Value}");
+            }
+            #endregion
+
+            Console.WriteLine("\n----------\n");
+
+            #region Déclaration d'un dictionnaire avec des textes.
+            Dictionary<string, string> auteurs = new Dictionary<string, string>()
+            {
+                { "Annie et Jean-Pierre Mahé", "Histoire de l'Arménie des origines à nos jours" },
+                { "Joasiah Ober", "L'énigme grecque" },
+                { "Hélène Montardre", "La mythologie grecque" }
+            };
+
+            foreach (KeyValuePair<string, string> element in auteurs)
+            {
+                Console.WriteLine($"{element.Key} - {element.Value}");
+            }
+            #endregion
+        }
+    }
+}
