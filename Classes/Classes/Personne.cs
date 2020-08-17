94553ab97ccd27e4d4d5972ca679a54aca71769e
unknown <contact@araikmadaryan.dev>
2020-08-17 04:31:50 +0200

    Ajout du dossier : Classes

---
+++ Classes/Classes/Personne.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+    public class Personne
+    {
+        private string nom;
+        private string prenom;
+        public int age { get; set; } // Accesseurs (getters) et mutateurs (setters).
+        public Personne(string nom, string prenom, int age)
+        {
+            this.nom = nom.ToUpper();
+            this.prenom = prenom[0].ToString().ToUpper() + prenom.Substring(1, prenom.Length - 1);
+            this.age = age;
+        }
+
+        public string Affichage()
+        {
+            return $"{prenom} {nom} a {age} ans.";
+        }
+    }
+}
---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personne unePersonne;
+            unePersonne = new Personne("pap", "nicolas", 21);
+            unePersonne.age = 24;
+            Console.WriteLine(unePersonne.Affichage());
+            Console.WriteLine(unePersonne.age); // On récupère la valeur "age".
+        }
+    }
+}
