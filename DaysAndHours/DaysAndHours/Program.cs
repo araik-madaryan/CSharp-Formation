17a3238e4f861e2ae51ed29caef8e47dcea6f1f3
unknown <contact@araikmadaryan.dev>
2020-08-29 22:54:53 +0200

    Dire bonjour, bonsoir ou bon week-end en fonction du moment de la journée

---
+++ DaysAndHours/DaysAndHours/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DaysAndHours
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime Date = DateTime.Now;
+
+            if (Date.DayOfWeek == DayOfWeek.Saturday || Date.DayOfWeek == DayOfWeek.Sunday) // SI le jour est égal à samedi OU dimanche
+            {
+                Weekend(); // AFFICHER bon week-end.
+            }
+            else if ((Date.Hour >= 18 && Date.DayOfWeek == DayOfWeek.Friday) || (Date.Hour < 9 && Date.DayOfWeek == DayOfWeek.Monday)) // SINON SI le jour est égal à vendredi à partir de 18H+ OU le jour est égal à lundi à moins de 9H
+            {
+                Weekend(); // AFFICHER bon week-end.
+            }
+            else if (Date.Hour >= 9 && Date.Hour < 18) // SINON SI l'heure est inclu entre 9H ET 18H
+            {
+                Bonjour(); // AFFICHER bonjour.
+            }
+            else // SINON
+            {
+                Bonsoir(); // AFFICHER bonsoir.
+            }
+        }
+
+        static void Bonjour() => Console.WriteLine($"Bonjour { Environment.UserName } !");
+        static void Bonsoir() => Console.WriteLine($"Bonsoir { Environment.UserName } !");
+        static void Weekend() => Console.WriteLine($"Bon week-end { Environment.UserName } !");
+    }
+}
