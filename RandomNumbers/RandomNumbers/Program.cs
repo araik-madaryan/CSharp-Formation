7d43acd23df7eda45142824f9c3f3bbf82598e4d
unknown <contact@araikmadaryan.dev>
2020-09-09 22:17:44 +0200

    Utilisation de valeurs aléatoires dans un tableau comportant 7 occurences dont les valeurs n'ont aucune égalité entre elles.

---
+++ RandomNumbers/RandomNumbers/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace RandomNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[7];
+            int i = 0;
+            while (i < array.Length)
+            {
+                int random = new Random().Next(1, 50);
+                if (!AlreadyUsed(i, random, array))
+                {
+                    array[i] = random;
+                    i++;
+                }
+            }
+            DisplayArray(array);
+        }
+
+        static bool AlreadyUsed(int i, int random, int[] array)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                if (array[j] == random)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        static void DisplayArray(int[] array)
+        {
+            Console.Write($"Le tirage au loto est le suivant : ");
+            foreach (int item in array)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
